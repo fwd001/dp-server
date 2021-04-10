caa85179a9877c77c006db19ed538ee7da4d8579
wendong <5917384949@qq.com>
2021-04-10 18:32:05 +0800

    feat: 添加 socket

--- app.js
+++ app.js
@@ -5,17 +5,28 @@ const app = new Koa()
 const router = new Router()
 // 创建实例 引入socket.io模块并绑定到服务器
 const server = require('http').createServer(app.callback())
-const io = require('socket.io')(server)
+const io = require('socket.io')(server, { cors: true })
 
 // 引入test路由
 const test = require('./router/api/test')
 
-
 const socketCallback = require('./socket/index')
 
-
 // socket连接
-io.on('connection', socketCallback);
+io.of('/client').on('connection', socketCallback);
+
+// 解决跨域
+app.use(async (ctx, next) => {
+    ctx.set('Access-Control-Allow-Origin', '*');
+    ctx.set('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+    ctx.set('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+    if (ctx.method == 'OPTIONS') {
+        ctx.body = 200;
+    } else {
+        await next();
+    }
+});
+
 
 // 路由监听
 router.use('/api', test)
@@ -26,6 +37,7 @@ router.get('/', async ctx => {
 
 // 配置路由
 app.use(router.routes()).use(router.allowedMethods())
+
 
 // 端口
 const port = process.env.PORT || 5000
--- socket/index.js
+++ socket/index.js
@@ -1,9 +1,9 @@
-module.exports =  (socket) => {
+module.exports = (socket) => {
     socket.on('chat message', (msg) => {
         console.log('message: ' + msg);
-        io.emit('chat message', msg);
+        socket.emit('chat message', '我收到了谢谢');
     });
     socket.on('disconnect', () => {
-        console.log('user disconnected');
+        console.log('user disconnected 断开连接');
     });
 }
